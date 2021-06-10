34d6fa9898bbe4d86da0fbebcea637607eb64c7b
Aryaman Pande <44070135+aryapande@users.noreply.github.com>
2021-06-11 01:10:11 +0530

    Create template.java

---
+++ PriorityQueue/template.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class HelloWorld {
+    
+    public static void main(String[] args) {
+        
+        PriorityQueue<Integer> p= new PriorityQueue<>();
+        p.add(30);
+        p.add(56);
+        p.add(3);
+        for(int x:p)
+        {   
+            System.out.println();
+            System.out.println(x);
+            System.out.println();
+        }
+        System.out.println(p.poll());
+        System.out.println(p.poll());
+        System.out.println(p.poll());
+        
+        
+    }
+}
